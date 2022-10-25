67a4fd0c825e6a87ad084664f436368b8a6f0395
chai <chaithra.amin98@gmail.com>
2022-10-25 16:56:30 +0530

    auto type text website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { TypeModifier } from '@angular/compiler';
 import { Component } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'auto-typing-website';
+  ngOnInit(){
+    //var autoText:any=document.querySelector("auto-text");
+   var type=new Typed(".auto-text",{
+    strings:["Coding","Sleeping","Eating"],
+    typeSpeed:150,
+    backSpeed:150,
+    loop:true
+   });
+
+  }
 }
